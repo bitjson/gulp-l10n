189db05be4fa0fa170d8110d4e3f41c2a5b83a9f
Jason Dreyzehner <jason@bitpay.com>
2015-03-01 15:07:25 -0500

    tests

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@
 
 var l10n = require('../');
 var assert = require('assert');
+var gutil = require('gulp-util');
 
 describe('gulp-l10n', function() {
 
@@ -11,6 +12,26 @@ describe('gulp-l10n', function() {
   });
 
   describe('extractLocale()', function() {
+
+    it('should extract locale strings from attributes', function(done) {
+      var stream = l10n.extractLocale({
+        attributes: ['alt', 'title', 'data-custom']
+      });
+      var test = new gutil.File({
+        contents: new Buffer('<html><body><img src="doesnotexist.gif" alt="contents of alt attribute" title="contents of title attribute" data-custom="custom attribute"></body></html>')
+      });
+      stream.once('data', function(file) {
+        assert(file.isBuffer());
+        assert.deepEqual(JSON.parse(String(file.contents)), {
+          "6cf62aab":"contents of alt attribute",
+          "65069d93":"contents of title attribute",
+          "89f8af89":"custom attribute"
+        });
+      });
+      stream.on('end', done);
+      stream.write(test);
+      stream.end();
+    });
 
   });
 
