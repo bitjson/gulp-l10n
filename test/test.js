5712e3fd1aff83b6cec9b89e0f9e1fe76cc8b28b
Jason Dreyzehner <jason@bitpay.com>
2015-03-01 16:46:40 -0500

    more tests for extractLocale method

--- test/test.js
+++ test/test.js
@@ -13,12 +13,12 @@ describe('gulp-l10n', function() {
 
   describe('extractLocale()', function() {
 
-    it('should extract locale strings from attributes', function(done) {
+    it('should extract locale strings from configured attributes', function(done) {
       var stream = l10n.extractLocale({
         attributes: ['alt', 'title', 'data-custom']
       });
       var test = new gutil.File({
-        contents: new Buffer('<html><body><img src="doesnotexist.gif" alt="contents of alt attribute" title="contents of title attribute" data-custom="custom attribute"></body></html>')
+        contents: new Buffer('<img src="example.gif" alt="contents of alt attribute" title="contents of title attribute" data-custom="custom attribute">')
       });
       stream.once('data', function(file) {
         assert(file.isBuffer());
@@ -27,6 +27,42 @@ describe('gulp-l10n', function() {
           "65069d93":"contents of title attribute",
           "89f8af89":"custom attribute"
         });
+      });
+      stream.on('end', done);
+      stream.write(test);
+      stream.end();
+    });
+
+    it('should extract locale strings from configured elements', function(done) {
+      var stream = l10n.extractLocale({
+        elements: ['p', 'custom']
+      });
+      var test = new gutil.File({
+        contents: new Buffer('<h1>heading</h1><p>This is a test.</p><custom>Custom element</custom>')
+      });
+      stream.once('data', function(file) {
+        assert(file.isBuffer());
+        assert.deepEqual(JSON.parse(String(file.contents)), {
+          "74251aeb":"Custom element",
+          "120ea8a2":"This is a test."});
+      });
+      stream.on('end', done);
+      stream.write(test);
+      stream.end();
+    });
+
+    it('should extract locale strings from elements with configured directives', function(done) {
+      var stream = l10n.extractLocale({
+        directives : ['localize', 'custom']
+      });
+      var test = new gutil.File({
+        contents: new Buffer('<div localize>This is a test.</div><div custom>custom directive</div>')
+      });
+      stream.once('data', function(file) {
+        assert(file.isBuffer());
+        assert.deepEqual(JSON.parse(String(file.contents)), {
+          "120ea8a2":"This is a test.",
+          "6bd3b8ac":"custom directive"});
       });
       stream.on('end', done);
       stream.write(test);
