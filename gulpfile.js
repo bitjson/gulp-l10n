bdff02247a220b893eaf548e01c04bbc74afbb38
Jason Dreyzehner <jason@bitpay.com>
2015-03-03 19:22:32 -0500

    switch npm test from
      gulp-l10n
        ✓ should have an extractLocale method
        ✓ should have a localize method
        ✓ should have a simulateTranslation method
        extractLocale()
          ✓ should extract locale strings from configured attributes
          ✓ should extract locale strings from configured elements
          ✓ should extract locale strings from elements with configured directives
          ✓ should allow other hash algorithms
          ✓ should allow for different hash lengths
          ✓ should allow for a different nativeLocale
        simulateTranslation()
          ✓ should simulate translation with a custom dictionary
          ✓ should not simulate translation of strings within html tags
    
      11 passing (34ms) to [19:22:32] Using gulpfile ~/Workspace/gulp-l10n/gulpfile.js
    [19:22:32] Starting 'test'...
    [19:22:32] Starting 'jshint'...
    [19:22:32] Finished 'jshint' after 214 ms
    [19:22:32] Starting 'mocha'...
    
      gulp-l10n
        ✓ should have an extractLocale method
        ✓ should have a localize method
        ✓ should have a simulateTranslation method
        extractLocale()
          ✓ should extract locale strings from configured attributes
          ✓ should extract locale strings from configured elements
          ✓ should extract locale strings from elements with configured directives
          ✓ should allow other hash algorithms
          ✓ should allow for different hash lengths
          ✓ should allow for a different nativeLocale
        simulateTranslation()
          ✓ should simulate translation with a custom dictionary
          ✓ should not simulate translation of strings within html tags
    
      11 passing (29ms)
    
    [19:22:32] Finished 'mocha' after 60 ms
    [19:22:32] Finished 'test' after 275 ms

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,25 @@
 
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
-var paths = ['*.js','test/**/*.js'];
+var runSequence = require('run-sequence');
+var js = '*.js';
+var tests = 'test/**/*.js';
 
-gulp.task('default', ['jshint'], function(cb){
-  gulp.watch([paths], ['jshint']);
+gulp.task('default', ['test'], function(cb){
+  gulp.watch([js, tests], ['test']);
+});
+
+gulp.task('test', function(cb){
+  runSequence('jshint', 'mocha', cb);
 });
 
 gulp.task('jshint', function () {
-  return gulp.src(paths)
+  return gulp.src([js, tests])
     .pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('mocha', function () {
+  return gulp.src(tests, {read: false})
+        .pipe($.mocha({reporter: 'spec'}));
 });
